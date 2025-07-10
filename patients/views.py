66a919e8105ed4fb86ce9ea0c297da6932304def
Samuel Vasquez <samivazqueles@gmail.com>
2025-07-09 21:13:37 -0500

    Agregar configuración de URLs y vistas para la gestión de pacientes en la aplicación de reservas

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Patient
+from .serializers import PatientSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def list_patients(request):
+    if request.method == 'GET':
+        patients = Patient.objects.all()
+        serializer = PatientSerializer(patients, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = PatientSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'POST'])
+def detail_patient(request, pk):
+    if request.method == 'GET':
+        try:
+            patient = Patient.objects.get(id=pk)
+        except Patient.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)

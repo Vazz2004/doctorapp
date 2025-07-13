75ddf6964a4c13abb930e964cb6a9f79da948e8f
Samuel Vasquez <samivazqueles@gmail.com>
2025-07-13 10:41:25 -0500

    Agregar vistas basadas en clase para listar y detallar doctores en la gestión de médicos

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Doctor
+from .serializers import DoctorSerializer
+from rest_framework.generics import  ListAPIView, CreateAPIView , RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+
+class ListoDoctor(ListAPIView,CreateAPIView):
+    allowed_method = ['GET', 'POST']
+    serializer_class= DoctorSerializer
+    queryset = Doctor.objects.all()
+
+
+class DetailDoctorView(RetrieveUpdateDestroyAPIView):
+    allowed_methods = ['GET', 'PUT', 'DELETE']
+    serializer_class = DoctorSerializer
+    queryset = Doctor.objects.all()
+

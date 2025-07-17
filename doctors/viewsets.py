6f28635e014dcb46a198bb1c273d3911536a6380
Samuel Vasquez <samivazqueles@gmail.com>
2025-07-16 20:00:39 -0500

    Agregar campo is_on_vacation al modelo Doctor y vista toggle_vacation en DoctorViewSet

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets
 from .serializers import DoctorSerializer
 from .models import Doctor
+from rest_framework.decorators import action
 
 class DoctorViewSet(viewsets.ModelViewSet):
     serializer_class= DoctorSerializer
     queryset = Doctor.objects.all()
     
+    @action(["POST"],detail=True)
+    def toggle_vacation(self,request,pk):
+       doctor = self.get_object()
+       if doctor.is_on_vacation:
+           doctor

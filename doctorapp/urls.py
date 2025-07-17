e0b51956dd969e03143f1d24a420a863839bb931
Samuel Vasquez <samivazqueles@gmail.com>
2025-07-16 21:05:59 -0500

    Agregar clases de autenticación en la configuración de REST framework y actualizar las URLs para incluir la autenticación

--- doctorapp/urls.py
+++ doctorapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
+
 urlpatterns = [
     path('admin/',admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('docs.urls')),
     path('api/patients/', include('patients.urls')),
     path('api/doctors/', include('doctors.urls')),
